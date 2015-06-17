3a6ac2a9ec4379f3531c3a4b77be60e1b64158bf
Manuel Riel <m@nuelriel.com>
2015-06-17 07:55:36 +0200

    Improve initial notebook update

--- deploy/00-update.py
+++ deploy/00-update.py
@@ -4,7 +4,7 @@ import threading
 def update_git():
     "Update sample notebooks and remove this file on first run."
     subprocess.Popen(["/usr/bin/git", "pull"], cwd="/home/swuser/notebooks/samples")
-    subprocess.Popen(["/bin/rm", "/home/swuser/.ipython/profile_default/startup/99-update.py"])
+    subprocess.Popen(["/bin/rm", "/home/swuser/.ipython/profile_default/startup/00-update.py"])
 
-t = Thread(target=update_git)
+t = threading.Thread(target=update_git)
 t.start()
