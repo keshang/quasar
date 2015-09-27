8f6264bb218ff4457028ddb24130fde15004a546
Manuel Riel <m@nuelriel.com>
2015-09-27 13:48:52 +0200

    Update or-tools, bump Java to 8.

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,12 @@ MAINTAINER riel@quantego.com
 
 ENV DEBIAN_FRONTEND noninteractive
 
+RUN sudo add-apt-repository -y ppa:openjdk-r/ppa
 RUN apt-get update && apt-get install -y net-tools inetutils-ping curl \
     git telnet socat tree unzip sudo software-properties-common python-mysqldb \
     pkg-config apt-utils wget build-essential python-dev python python-pip wget \
     liblapack-dev libatlas-dev gfortran libfreetype6 libfreetype6-dev libpng12-dev \
-    python-lxml libyaml-dev g++ libffi-dev libzmq-dev libzmq1 openjdk-7-jdk \
+    python-lxml libyaml-dev g++ libffi-dev libzmq-dev libzmq1 openjdk-8-jdk \
     glpk-utils coinor-cbc coinor-clp
 RUN pip install -U setuptools pip distribute configobj numpy
 
--- deploy/profiles/profile_default/startup/10-imports.py
+++ deploy/profiles/profile_default/startup/10-imports.py
@@ -16,11 +16,6 @@ import sqlobject
 import patsy
 import math
 
-# needed to print utf8-chars with pandas.
-# import sys
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
-
 #matplotlib.rcParams['figure.figsize'] = (3, 2)
 #matplotlib.rcParams['savefig.dpi'] = 100
 
@@ -28,19 +23,8 @@ from IPython.display import display
 from IPython.display import SVG
 from IPython.display import Image
 
-# from pyquasar import *
-
 # Sensible float formats
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 pd.set_option('mode.chained_assignment',None)
 np.set_printoptions(precision=2)
 np.set_printoptions(suppress=True)
-
-
-## pretty plots
-from mpltools import style
-style.use('ggplot')
-
-
-# import matplotlib as mpl
-# mpl.rcParams['axes.color_cycle'] = ('#002D6A', '#4B004B', '#9BC6E7', '#BE54BE', '#A8CA00', '#FF9200', '#613700')
